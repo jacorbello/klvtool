a1b36dff213a9fa8e4d45320bc275a7105765527
Jeremy Corbello <jacorbello@gmail.com>
2026-04-14 12:20:29 -0500

    fix: mask SIGPIPE to prevent broken pipe noise in pipelines

--- cmd/klvtool/main.go
+++ cmd/klvtool/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/jacorbello/klvtool/internal/cli"
 )
 
 func main() {
+	signal.Ignore(syscall.SIGPIPE)
 	os.Exit(cli.Main())
 }
